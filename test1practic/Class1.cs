50a6fb9561dc6348f1aa18197ebf8598e6c68c00
Andrey Ilin <isip_an.n.ilyin@mpt.ru>
2024-09-14 23:18:22 +0300

    1

---
+++ test1practic/Class1.cs
@@ -0,0 +1,54 @@
+﻿namespace test1practic
+{
+    public class Class1
+    {
+        public static int PasswordVerify(string password)
+        {
+            int result = 0;
+            if (password.Length >= 7)
+            {
+                result++;
+
+            }
+
+            foreach (char c in password)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    result++;
+                    break;
+                }
+            }
+
+            foreach (char c in password)
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    result++;
+                    break;
+                }
+            }
+
+            foreach (char c in password)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    result++;
+                    break;
+                }
+            }
+
+            char[] specialCharacters = "!@#$%^&*()_+-=[]{}|;':\",.<>?/`~".ToCharArray();
+            foreach (char c in password)
+            {
+                if (specialCharacters.Contains(c))
+                {
+                    result++;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ test1practicTests/Class1Tests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using test1practic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1practic.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void TestPassword_Empty_0()
+        {
+            Assert.AreEqual(5, Class1.PasswordVerify(""));
+        }
+        [TestMethod()]
+        public void TestPassword_Numbers_1()
+        {
+            Assert.AreEqual(5, Class1.PasswordVerify("1"));
+        }
+        [TestMethod()]
+
+        public void TestPassword_Letters_2()
+        {
+            Assert.AreEqual(5, Class1.PasswordVerify("1prac"));
+        }
+        [TestMethod()]
+
+        public void TestPassword_BigLetters_3()
+        {
+            Assert.AreEqual(5, Class1.PasswordVerify("1Prac"));
+        }
+        [TestMethod()]
+
+        public void TestPassword_Length_6()
+        {
+            Assert.AreEqual(5, Class1.PasswordVerify("Practic"));
+        }
+
+        public void TestPassword_Special_4()
+        {
+            Assert.AreEqual(5, Class1.PasswordVerify("1Prac-"));
+        }
+        [TestMethod()]
+
+        public void TestPassword_Final_5()
+        {
+            Assert.AreEqual(5, Class1.PasswordVerify("1Practic!123"));
+        }
+    }
+}
